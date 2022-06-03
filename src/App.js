3b59298b355f7874fc97fb2722111f0a98c09b58
ProSid2002 <siddharthsharmads@gmail.com>
2022-06-04 01:40:49 +0530

    Updated project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getJobOpenings } from "./data/jobOpenings";
+import { getUserDetails } from "./data/userDetails";
+import "./css/styles.css";
 
-function App() {
+const App = () => {
+  const [jobData, setJobData] = useState([]);
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    getUserDetails(setUserData);
+    getJobOpenings(setJobData);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="user">
+        <h1>My Profile</h1>
+        <div>
+          <img src={userData.pictureUrl} alt="" />
+          <h2>
+            <span className="bold">NAME:</span> {userData.name}
+          </h2>
+          <h2>
+            <span className="bold">COLLEGE:</span> {userData.college}
+          </h2>
+        </div>
+      </div>
+      <div className="jobs">
+        <h1>Job Openings</h1>
+        {jobData.map(
+          ({ id, company, designation, location, min_experience, skills }) => (
+            <div className="opening" key={id}>
+              <h2>{company}</h2>
+              <h3>
+                <span className="bold">Designation:</span> {designation}
+              </h3>
+              <h3>
+                <span className="bold">Job Location:</span> {location}
+              </h3>
+              <h3>
+                <span className="bold">Experience Required:</span>{" "}
+                {min_experience === 0
+                  ? "Fresher"
+                  : min_experience === 1
+                  ? min_experience + " year"
+                  : min_experience + " years"}
+              </h3>
+              <h3>
+                <span className="bold">Skills:</span>{" "}
+                {skills.map((item) => (
+                  <span className="skill">{item}&nbsp;</span>
+                ))}
+              </h3>
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
